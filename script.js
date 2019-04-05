e50150d3a5bc7b9889fa1dc13d64bc44c89a7223
David Wyseur <david.wyseur@gmail.com>
2019-04-05 19:25:06 +0200

    cleaning code

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
 'use strict';
 
 
-// gérer le clic sur le bouton "ajouter" ou la touche "Enter"
-// récupérer le texte à ajouter dans la liste
-// ajouter le texte à la liste
-
-/*
-let aaa = document.getElementsByName('newElem')[0];
-aaa.addEventListener('keydown', (e) => {
-	console.log(`Touche du clavier = ${e.key}`);
-});
-*/
-
-//document.getElementsByName('newElem')[0].addEventListener('keydown', action => { console.log(`Touche = ${action.key}`) });
-
 document.getElementsByName('newElem')[0].addEventListener('keydown', action => { (action.key == 'Enter') ? addList() : null });
-/*let keyboard = document.getElementsByName('newElem')[0].addEventListener('keydown', action => { action.key });
-console.log(keyboard);
-if(keyboard == 'Enter'){
-	addList();
-}*/
-
 
 function addList() {
 
