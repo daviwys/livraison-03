692ed45c536852b08929c91b309f1b1984c8f9b5
David Wyseur <david.wyseur@gmail.com>
2019-04-05 19:19:27 +0200

    action on key Enter : done !

--- script.js
+++ script.js
@@ -5,6 +5,21 @@
 // récupérer le texte à ajouter dans la liste
 // ajouter le texte à la liste
 
+/*
+let aaa = document.getElementsByName('newElem')[0];
+aaa.addEventListener('keydown', (e) => {
+	console.log(`Touche du clavier = ${e.key}`);
+});
+*/
+
+//document.getElementsByName('newElem')[0].addEventListener('keydown', action => { console.log(`Touche = ${action.key}`) });
+
+document.getElementsByName('newElem')[0].addEventListener('keydown', action => { (action.key == 'Enter') ? addList() : null });
+/*let keyboard = document.getElementsByName('newElem')[0].addEventListener('keydown', action => { action.key });
+console.log(keyboard);
+if(keyboard == 'Enter'){
+	addList();
+}*/
 
 
 function addList() {
@@ -12,6 +27,9 @@ function addList() {
 	let elem = document.getElementsByName('newElem')[0].value;
 	//console.log(elem);
 	
+	// Capitalize 1st letter
+	elem = capitalize(elem);
+
 	// destination of new element
 	let ul = document.getElementsByTagName('ul')[0];
 	// Create new element
@@ -23,4 +41,12 @@ function addList() {
 
 	// Empty field
 	document.getElementsByName('newElem')[0].value='';
+}
+
+function capitalize(str) {
+
+	let strTab = str.split('');
+	strTab[0] = strTab[0].toUpperCase();
+	return strTab.join('');
+
 }
